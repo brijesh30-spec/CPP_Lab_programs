da58265df1b040daeb5088c077c6b7039f9d4765
brijesh30-spec <brijeshmr399@gmail.com>
2026-09-11 11:00:55 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int main()
+{
+    vector<string> msg {"hello","c++","world","from","vs code","änd the c++ extemsion"};
+    for(const string& word: msg)
+    {
+        cout<< word <<" ";
+    }
+    {
+        cout << endl;
+    }
+    }
